0888cabb0d770bfa661757d464f61ced3f739b8f
Kevin MacFadyen <kmacfadyen3@gmail.com>
2023-04-25 14:40:01 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // import the server and start it!
+const server = require('./api/server.js')
+
+// Launch server
+server.listen(9000, () => {
+    console.log(`server started on http://localhost:9000`)
+})
+// Can be manually started by node index.js
+// But we can make it update automatically
+
+// Installing nodemon will do just that!
+// Installed as a dev dependency (npm i -D nodemon)
